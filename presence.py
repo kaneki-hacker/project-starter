09f0eeca8f7919c792d1ea559782f2db25ac4370
Yusei <killer12hunter1@gmail.com>
2024-06-12 01:47:33 +0100

    Made the presence client working

--- presence.py
+++ presence.py
@@ -6,23 +6,41 @@ import os
 import time
 import psutil
 import typing
+import sys
 
 # Declaring variables
 client = os.getenv("DISCORD_APP_CLIENT")
 
 # Creating functions
 def is_discord_running() -> bool:
-    return false
+    for proc in psutil.process_iter(["pid","name"]):
+        try:
+            if proc.name().lower() == "discord":
+                print("dis is running")
+                return True
+        except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.AccessDenied):
+            pass
+    print("dis is not running")
+    return False
 
 def run_discord():
-    os.system("")
+    os.system("discord 1>/dev/null 2>/dev/null &")
+
+def presence_connect():
+    RPC=pypresence.Presence(client)
+    RPC.connect()
+    starting_time=time.time()
+    RPC.update(state="WORKING...", party_size=[1,1], start=int(starting_time), large_text="Coding...",
+               small_text="Creating awesome projects!!")
 
 # Main function
 def main():
     is_D_running = is_discord_running()
-    if !is_D_running:
+    if is_D_running == False:
         run_discord()
-
+    presence_connect()
+    while True:
+        time.sleep(30)
 
 # Main file entry
 if __name__ == "__main__":
